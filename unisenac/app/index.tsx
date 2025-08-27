e8b4be37930a984e3206e5c0747d46f55e233c7d
Wagner Loch <wagner_loch@hotmail.com>
2025-08-27 09:39:55 -0300

    Repassando parametros em rotas de forma dinamica

--- unisenac/app/index.tsx
+++ unisenac/app/index.tsx
@@ -13,6 +13,14 @@ export default function Index() {
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text> FALA PESSOAL!!!! </Text>
       <Link href={"/config"}>IR PARA CONFIGURAÇÕES</Link>
+      <Link href={{
+        pathname: "/itens/[id]",
+        params: {
+          id: 123,
+          name: "Item 123",
+          description: "Descrição do Item 123"
+        }
+      }}>Ir para a tela do Item 123</Link>
     </View>
   );
 }
--- unisenac/app/itens/[id].tsx
+++ unisenac/app/itens/[id].tsx
@@ -2,11 +2,13 @@ import { useLocalSearchParams } from 'expo-router';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function DetailsScreen() {
-  const { id } = useLocalSearchParams();
+  const { id, name, description } = useLocalSearchParams();
 
   return (
     <View style={styles.container}>
       <Text>Detalhes do item {id} </Text>
+      <Text>Nome: {name} </Text>
+      <Text>Descrição: {description} </Text>
     </View>
   );
 }
